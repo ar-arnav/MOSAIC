236f20fc25ee1e03c0b85113332c7130a6560782
Arnav <arnavshirkear@gmail.com>
2026-08-26 07:56:00 +0000

    Added GW processing

---
+++ src/gw_processor.py
@@ -0,0 +1,78 @@
+import os
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import healpy as hp
+from astropy.table import Table
+import astropy_healpix as ah
+from astropy.coordinates import SkyCoord
+
+
+class GW_data:
+    def __init__(self, GW_data_path):
+        self.GW_data = Table.read(GW_data_path)
+
+        self.uniq = np.array(self.GW_data['UNIQ'], dtype=np.int64)
+        level, ipix = ah.uniq_to_level_ipix(self.uniq)
+        self.min_level = np.min(level)
+        self.max_level = np.max(level)
+
+        unique_levels, counts = np.unique(level, return_counts=True)
+
+        level_nside = ah.level_to_nside(level)
+        level_pixels = ah.nside_to_pixel_area(level_nside)
+
+        probdensity = self.GW_data['PROBDENSITY'] 
+
+        self.pixel_prob = probdensity * level_pixels
+
+        prob_sorted = np.argsort(self.pixel_prob)[::-1]
+        cumsum_result = np.cumsum(self.pixel_prob[prob_sorted])
+        total_prob = cumsum_result / cumsum_result[-1]
+
+        self.credible_levels = np.zeros(len(self.pixel_prob))
+        self.credible_levels[prob_sorted] = total_prob
+
+    def evaluate_candidate(self, ra_deg, dec_deg, dist_mpc):
+        row_idx = None
+        
+        Skycoord = SkyCoord(ra=ra_deg, dec=dec_deg, unit='deg', frame='icrs')
+    
+        nside = ah.level_to_nside(self.max_level)
+        target_ipix = ah.lonlat_to_healpix(Skycoord.ra, Skycoord.dec, nside, order='nested')
+    
+        target_uniq = ah.level_ipix_to_uniq(self.max_level, target_ipix)
+    
+        idx = np.searchsorted(self.uniq, target_uniq)
+    
+        if self.uniq[idx] == target_uniq:
+            row_idx = idx
+        else:
+            curr_level = self.max_level
+            curr_ipix = target_ipix
+    
+            while curr_level >= self.min_level:
+            
+                curr_ipix = curr_ipix//4
+                
+                curr_level -= 1
+    
+                curr_uniq = ah.level_ipix_to_uniq(curr_level, curr_ipix)
+                
+                curr_idx = np.searchsorted(self.uniq, curr_uniq)
+                if curr_idx < len(self.uniq) and self.uniq[curr_idx] == curr_uniq:
+                    row_idx = curr_idx
+                    break
+    
+        mu = self.GW_data['DISTMU'][row_idx]
+        sigma = self.GW_data['DISTSIGMA'][row_idx]
+        norm = self.GW_data['DISTNORM'][row_idx]
+        prob_2d = self.GW_data['PROBDENSITY'][row_idx]
+    
+        gaussian = 1/ (np.sqrt(2*np.pi)*sigma) * np.exp(-0.5*((dist_mpc - mu)/(sigma))**2)
+        dp_dv = norm * gaussian * prob_2d
+        return self.credible_levels[row_idx], dp_dv
+
+
+mapper = GW_data("~/MOSAIC/data/raw/GW/0.fits")
+print(mapper.evaluate_candidate(15.0, 80.0, 500.0))
